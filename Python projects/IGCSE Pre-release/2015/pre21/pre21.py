4a7767c8d4071a085f13fd47db1b280eb1325632
Shiramiki <123367383+Shiramiki@users.noreply.github.com>
2026-01-05 12:54:25 +0300

    Done Task 2

--- Python projects/IGCSE Pre-release/2015/pre21/pre21.py
+++ Python projects/IGCSE Pre-release/2015/pre21/pre21.py
@@ -1,4 +1,7 @@
 day = int(input("Please enter the number of days in the month:"))
+middayTotal = 0
+midnightTotal = 0
+
 while day < 28 or day > 31:
     day = int(input("Invalid input. Please enter a number between 28 and 31:"))
 midday = [None]*day
@@ -13,6 +16,7 @@ for days in range(0, day):
     while midnights < -30 or midnights > 50:
         midnights=int(input(f"Invalid input. Please enter the midnight temperature for day {days + 1} between -30 and 50:"))
     middayTotal += middays
+    midnightTotal += midnights
     midnight[days] = midnights
     midday[days] =middays
     # Today distracted me so I could not finish the last part of the code.
@@ -21,4 +25,5 @@ print("Day\tMidday\tMidnight")
 for i in range(day):
     print(f"{i + 1}\t{midday[i]}\t{midnight[i]}")
 
-
+print(f"Average midday temperature: {middayTotal/day}")
+print(f"Average midnight temperature: {midnightTotal/day}")
