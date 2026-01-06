3dd50e20706a4f8eb6ac4d23ca3685c923cbbcba
Shiramiki <123367383+Shiramiki@users.noreply.github.com>
2026-01-06 09:11:27 +0300

    Add try except

--- Python projects/IGCSE Pre-release/2015/pre21/pre21.py
+++ Python projects/IGCSE Pre-release/2015/pre21/pre21.py
@@ -1,25 +1,45 @@
-day = int(input("Please enter the number of days in the month:"))
+
 middayTotal = 0
 midnightTotal = 0
 
-while day < 28 or day > 31:
-    day = int(input("Invalid input. Please enter a number between 28 and 31:"))
+
 midday = [None]*day
 midnight = [None]*day
 
-for days in range(0, day):
-    middays=int(input(f"Please enter the midday temperature for day {days + 1}:"))
-    while middays < -30 or middays > 50:
-        middays=int(input(f"Invalid input. Please enter the midday temperature for day {days + 1} between -30 and 50:"))    
 
-    midnights=int(input(f"Please enter the midnight temperature for day {days + 1}:"))
-    while midnights < -30 or midnights > 50:
-        midnights=int(input(f"Invalid input. Please enter the midnight temperature for day {days + 1} between -30 and 50:"))
+try:
+    day = int(input("Please enter the number of days in the month:"))
+    while day < 28 or day > 31:
+        day = int(input("Invalid input. Please enter a number between 28 and 31:"))
+except:
+    print("Invalid input. Please enter a valid integer.")
+    exit()
+
+
+
+for days in range(0, day):
+    try:
+        middays=int(input(f"Please enter the midday temperature for day {days + 1}:"))
+        while middays < -30 or middays > 50:
+            middays=int(input(f"Invalid input. Please enter the midday temperature for day {days + 1} between -30 and 50:"))   
+    except:
+        print("Invalid input. Please enter a valid integer.")
+        exit() 
+    try:
+        midnights=int(input(f"Please enter the midnight temperature for day {days + 1}:"))
+        while midnights < -30 or midnights > 50:
+            midnights=int(input(f"Invalid input. Please enter the midnight temperature for day {days + 1} between -30 and 50:"))
+    except:
+        print("Invalid input. Please enter a valid integer.")
+        exit()
+
+
     middayTotal += middays
     midnightTotal += midnights
+
     midnight[days] = midnights
     midday[days] =middays
-    # Today distracted me so I could not finish the last part of the code.
+
 
 print("Day\tMidday\tMidnight")
 for i in range(day):
