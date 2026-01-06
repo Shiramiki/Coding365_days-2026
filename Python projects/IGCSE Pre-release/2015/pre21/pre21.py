d5513fed9f22e936798198ff41c032b15d0a0a28
Shiramiki <123367383+Shiramiki@users.noreply.github.com>
2026-01-06 09:39:37 +0300

    updated try except

--- Python projects/IGCSE Pre-release/2015/pre21/pre21.py
+++ Python projects/IGCSE Pre-release/2015/pre21/pre21.py
@@ -2,27 +2,39 @@
 middayTotal = 0
 midnightTotal = 0
 
-try:
-    day = int(input("Please enter the number of days in the month:"))
-    while day < 28 or day > 31:
-        day = int(input("Invalid input. Please enter a number between 28 and 31:"))
-except:
-    print("Invalid input. Please enter a valid integer.")
-    exit()
 
-midday = [None]*day
-midnight = [None]*day
-
-
-
-for days in range(0, day):
+def dayInMonth():
+    global day, midday, midnight
     try:
-        middays=int(input(f"Please enter the midday temperature for day {days + 1}:"))
-        while middays < -30 or middays > 50:
-            middays=int(input(f"Invalid input. Please enter the midday temperature for day {days + 1} between -30 and 50:"))   
+        day = int(input("Please enter the number of days in the month:"))
+        while day < 28 or day > 31:
+            day = int(input("Invalid input. Please enter a number between 28 and 31:"))
+    except:
+        print("Invalid input. Please enter a valid integer.")
+        exit()
+
+    else:
+        midday = [None]*day
+        midnight = [None]*day
+
+
+def tempMiddayValues():
+    try:
+            middays=int(input(f"Please enter the midday temperature for day {days + 1}:"))
+            while middays < -30 or middays > 50:
+                middays=int(input(f"Invalid input. Please enter the midday temperature for day {days + 1} between -30 and 50:"))   
     except:
         print("Invalid input. Please enter a valid integer.")
         exit() 
+        tempMiddayValues()
+        
+    else:
+        
+        midnights =tempMidnightValues()
+        temp =[middays, midnights]
+        return temp
+
+def tempMidnightValues():
     try:
         midnights=int(input(f"Please enter the midnight temperature for day {days + 1}:"))
         while midnights < -30 or midnights > 50:
@@ -30,18 +42,27 @@ for days in range(0, day):
     except:
         print("Invalid input. Please enter a valid integer.")
         exit()
+        tempMidnightValues()
+    else:
+        return midnights
 
 
-    middayTotal += middays
-    midnightTotal += midnights
 
-    midnight[days] = midnights
-    midday[days] =middays
+
+
+for days in range(0, day):
+    temp = tempMiddayValues()
+    midnight[days] = temp.pop()  # Get the last element which is
+    midday[days] = temp.pop()    # Get the first element which is
+    middayTotal += midday[days]
+    middayAvg = middayTotal / day
+    midnightTotal += midnight[days]
+    midnightAvg = midnightTotal / day
 
 
 print("Day\tMidday\tMidnight")
 for i in range(day):
     print(f"{i + 1}\t{midday[i]}\t{midnight[i]}")
 
-print(f"Average midday temperature: {middayTotal/day}")
-print(f"Average midnight temperature: {midnightTotal/day}")
+print(f"Average midday temperature: {middayAvg}")
+print(f"Average midnight temperature: {midnightAvg}")
