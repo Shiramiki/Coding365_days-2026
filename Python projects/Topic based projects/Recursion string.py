caae5fb421e52653462a93ffa04457cea637b6b5
Shiramiki <123367383+Shiramiki@users.noreply.github.com>
2026-01-11 22:41:48 +0300

    Additions

---
+++ Python projects/Topic based projects/Recursion string.py
@@ -0,0 +1,10 @@
+sentence ="Hello, welcome to the world of recursion!"
+
+def reverse_string(s):
+
+    if len(s) == 0:
+        return s
+    else:
+        return s[-1] + reverse_string(s[:-1])
+
+    
