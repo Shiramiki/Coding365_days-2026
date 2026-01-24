054a8c3df3f5fa5a14cda3f0037008cd1af64831
Shiramiki <123367383+Shiramiki@users.noreply.github.com>
2026-01-24 08:36:41 +0300

    creating database and table

--- Python projects/IGCSE Pre-release/2015/pre21/OOP/pre21sql.py
+++ Python projects/IGCSE Pre-release/2015/pre21/OOP/pre21sql.py
@@ -3,6 +3,16 @@ mydb =mysql.connector.connect(
     host="localhost",
     user ="user",
     password ="MyStrongPass123",
+    database="pre21sql"
 )
 
 print(mydb)
+mycursor = mydb.cursor() #object that communicates with entire databas
+
+# mycursor.execute("CREATE DATABASE pre21sql")
+
+mycursor.execute("SHOW TABLES")
+for db in mycursor:
+    print(db)
+
+# mycursor.execute("CREATE TABLE tempdata (time VARCHAR(255), temp FLOAT)")
